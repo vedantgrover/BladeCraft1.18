3800f6a38edaa1fe897aef7cc351f39f6f93f1ce
bladedurman <vedantvgrover@gmail.com>
2022-06-11 20:51:54 -0700

    Added Ruby Item

--- src/main/java/com/bladedurman/bladecraft/BladeCraft.java
+++ src/main/java/com/bladedurman/bladecraft/BladeCraft.java
@@ -1,7 +1,9 @@
 package com.bladedurman.bladecraft;
 
+import com.bladedurman.bladecraft.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -17,8 +19,10 @@ public class BladeCraft {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public BladeCraft() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+
+        ModItems.register(eventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/com/bladedurman/bladecraft/item/ModItems.java
+++ src/main/java/com/bladedurman/bladecraft/item/ModItems.java
@@ -1,12 +1,19 @@
 package com.bladedurman.bladecraft.item;
 
 import com.bladedurman.bladecraft.BladeCraft;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BladeCraft.MOD_ID);
 
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
