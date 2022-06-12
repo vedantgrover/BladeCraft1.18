a0a3bb6fd42176ff62bf15bbe063b6ca6423a481
bladedurman <vedantvgrover@gmail.com>
2022-06-11 20:24:49 -0700

    Added Items Registries

---
+++ src/main/java/com/bladedurman/bladecraft/item/ModItems.java
@@ -0,0 +1,12 @@
+package com.bladedurman.bladecraft.item;
+
+import com.bladedurman.bladecraft.BladeCraft;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BladeCraft.MOD_ID);
+
+
+}
