be9cc80f7ebca21302db6353019673d6fad9f4ee
bladedurman <vedantvgrover@gmail.com>
2022-06-12 18:27:02 -0700

    Added Sapphires

--- src/main/java/com/bladedurman/bladecraft/item/ModItems.java
+++ src/main/java/com/bladedurman/bladecraft/item/ModItems.java
@@ -11,7 +11,9 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BladeCraft.MOD_ID);
 
+    // Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
